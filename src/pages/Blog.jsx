7bd839492d23b7c72ec7478b77e3d269db584078
sebastian-huamani <huamanitassar@mail.com>
2023-02-21 21:28:24 -0500

    blogs

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -32,9 +32,9 @@ class Blog extends React.Component {
 
             <div className='text-justify  sm:text-left flex items-center '>
               <div>
-                <h1 className='font-semibold text-lg'>Plantillas</h1>
+                <h1 className='font-semibold text-lg'>Plantillas <sup>(proximamente)</sup></h1>
                 Como crear una? Para que sirve? Como mantener la organizacion? y mas respuestas sobre las plantillas
-                <div className='w-full text-end mt-6'><NavLink to="/Blog/Categorias" className=' border-b border-gray-600 hover:pb-1 ease-in-out duration-200'>Leer</NavLink></div>
+                <div className='w-full text-end mt-6'><NavLink  className=' border-b border-gray-600 hover:pb-1 ease-in-out duration-200'>Leer</NavLink></div>
               </div>
             </div>
 
@@ -45,40 +45,30 @@ class Blog extends React.Component {
 
             <div className='text-justify  sm:text-left flex items-center '>
               <div>
+                <h1 className='font-semibold text-lg'>Transacciones <sup>(proximamente)</sup></h1>
                 Lorem  ipsum dolor sit amet consectetur adipisicing elit. Saepe modi maiores aut molestiae dolore ullam?
-                <div className='w-full text-end mt-6'><NavLink to="/Blog/BlogA" className='border-b border-gray-600 hover:pb-1 ease-in-out duration-200'>Leer</NavLink></div>
+                <div className='w-full text-end mt-6'><NavLink  className='border-b border-gray-600 hover:pb-1 ease-in-out duration-200'>Leer</NavLink></div>
               </div>
             </div>
 
-            <div className=' bg-gray-800 h-48 mt-4 md:mt-0'>
-            </div>
+            <img src="https://i.pinimg.com/564x/5b/9d/43/5b9d43e07d8714f7c3cb94e907bba5c7.jpg" alt="" className='w-full object-cover object-center h-48 mt-4 md:mt-0' />
+
           </div>
 
           <div className='mt-6 sm:block  md:grid grid-cols-2 gap-32 justify-between border-x-2 border-gray-300 px-6 sm:px-16 '>
 
             <div className='text-justify  sm:text-left flex items-center '>
               <div>
+                <h1 className='font-semibold text-lg'>Prestamos <sup>(proximamente)</sup></h1>
                 Lorem  ipsum dolor sit amet consectetur adipisicing elit. Saepe modi maiores aut molestiae dolore ullam?
-                <div className='w-full text-end mt-6'><NavLink to="/Blog/BlogA" className='border-b border-gray-600 hover:pb-1 ease-in-out duration-200'>Leer</NavLink></div>
+                <div className='w-full text-end mt-6'><NavLink  className='border-b border-gray-600 hover:pb-1 ease-in-out duration-200'>Leer</NavLink></div>
               </div>
             </div>
 
-            <div className=' bg-gray-800 h-48 mt-4 md:mt-0'>
-            </div>
+            <img src="https://images.pexels.com/photos/210990/pexels-photo-210990.jpeg?auto=compress&cs=tinysrgb&w=1260&h=1260&dpr=1" alt="" className='w-full object-cover 
+            object-center h-48 mt-4 md:mt-0' />
           </div>
 
-          <div className='mt-6 sm:block  md:grid grid-cols-2 gap-32 justify-between border-x-2 border-gray-300 px-6 sm:px-16 '>
-
-            <div className='text-justify  sm:text-left flex items-center '>
-              <div>
-                Lorem  ipsum dolor sit amet consectetur adipisicing elit. Saepe modi maiores aut molestiae dolore ullam?
-                <div className='w-full text-end mt-6'><NavLink to="/Blog/BlogA" className='border-b border-gray-600 hover:pb-1 ease-in-out duration-200'>Leer</NavLink></div>
-              </div>
-            </div>
-
-            <div className=' bg-gray-800 h-48 mt-4 md:mt-0'>
-            </div>
-          </div>
         </div>
 
         <Footer />
