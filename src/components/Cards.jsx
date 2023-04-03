dbce59a9b56d09184a03cfb81ad49ca76a998d12
sebastian-huamani <huamanitassar@mail.com>
2023-04-03 11:18:45 -0500

    cads

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -5,6 +5,7 @@ import Card from "../components/Card";
 import CardContext from "../context/CardContext";
 import Loading from "./Loading";
 import NotData from "./NotData";
+import Error from './Error';
 
 
 
@@ -12,34 +13,89 @@ class Cards extends React.Component {
     constructor(props) {
         super(props);
         this.state = ({
-            done: false,
-            items: [],
+            res: false,
+            loading: true,
+            data: [],
+            error: false
         });
     }
 
     componentDidMount() {
         let key = localStorage.getItem('key');
 
-        const fetchPromise = fetch("https://financemeapi.com/api/cards", {
+        fetch("https://financemeapi.com/api/cards", {
+            method: 'GET',
             'headers': {
                 'Authorization': 'Bearer ' + key,
+                'Content-Type': 'text/plain',
+                'Accept': 'application/json',
             }
-        });
-
-        fetchPromise.then(response => {
-            return response.json();
-        }).then(res => {
-            this.setState({
-                items: res['msg'],
-                done: res['res']
+        }, [])
+            .then(response => response.json())
+            .then(res => {
+                this.setState({
+                    res: res['res'],
+                    loading: res['res'] ? false : true,
+                    error: res['res'] ? false : true,
+                    data: res['msg'],
+                });
+                this.context.updateCardsList(res['msg']);
             });
-            this.context.updateCardsList(res['msg']);
-        }, []);
     }
 
     render() {
-        var { items, done } = this.state;
+        var { res, loading, error, data } = this.state;
         var { CardList } = this.context;
+
+        if (error) {
+            return (
+                <div className='h-80 sm:h-90vh'>
+                    <div className='my-4 flex justify-end text-center text-lg'>
+                        <NavLink to="/Dashboard/Crear-Nueva-Tarjeta" >
+                            <FiPlusCircle className='mx-3 cursor-cell' />
+                        </NavLink>
+                    </div>
+
+                    <div className='flex items-center justify-center h-full text-8xl opacity-25'>
+                        <Error />
+                    </div>
+                </div>
+            );
+        }
+
+        if (loading) {
+            return (
+                <div className='h-80 sm:h-90vh'>
+                    <div className='my-4 flex justify-end text-center text-lg'>
+                        <NavLink to="/Dashboard/Crear-Nueva-Tarjeta" >
+                            <FiPlusCircle className='mx-3 cursor-cell' />
+                        </NavLink>
+                    </div>
+
+                    <div className='overflow-y-auto h-full pr-1 animate-pulse'>
+                        <div className='w-full flex flex-col h-32'>
+                            <p className='bg-zinc-200 h-5/6 rounded-t w-full'></p>
+                            <p className='bg-slate-300 h-2/6 rounded-b w-full'></p>
+                        </div>
+
+                        <div className='w-full flex flex-col h-32 mt-4'>
+                            <p className='bg-zinc-200 h-5/6 rounded-t w-full'></p>
+                            <p className='bg-slate-300 h-2/6 rounded-b w-full'></p>
+                        </div>
+
+                        <div className='w-full flex flex-col h-32 mt-4'>
+                            <p className='bg-zinc-200 h-5/6 rounded-t w-full'></p>
+                            <p className='bg-slate-300 h-2/6 rounded-b w-full'></p>
+                        </div>
+
+                        <div className='w-full flex flex-col h-32 mt-4'>
+                            <p className='bg-zinc-200 h-5/6 rounded-t w-full'></p>
+                            <p className='bg-slate-300 h-2/6 rounded-b w-full'></p>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
 
         return (
             <div className='h-80 sm:h-90vh'>
@@ -51,11 +107,11 @@ class Cards extends React.Component {
 
                 <div className='overflow-y-auto h-full text-xs pr-1'>
 
-                    {!done ? <Loading /> : (items.length == 0 ?
+                    {data.length == 0 ?
                         <NotData >
                             <div className='mt-2' >
-                                <NavLink  className='btn' to="/Dashboard/Crear-Nueva-Tarjeta">
-                                    Crear Una Tarjeta 
+                                <NavLink className='btn' to="/Dashboard/Crear-Nueva-Tarjeta">
+                                    Crear Una Tarjeta
                                 </NavLink>
                             </div>
                         </NotData>
@@ -65,7 +121,6 @@ class Cards extends React.Component {
                                 key={item.id}
                                 dataItem={item}
                             />
-                        )
                         ))
                     }
                 </div>
