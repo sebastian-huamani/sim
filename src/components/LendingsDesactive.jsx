0fc67a3b8e75751ae2d5f8d9aa5241289dc462c0
sebastian-huamani <huamanitassar@mail.com>
2023-04-04 10:56:40 -0500

    lending

--- src/components/LendingsDesactive.jsx
+++ src/components/LendingsDesactive.jsx
@@ -4,13 +4,16 @@ import Loading from "./Loading";
 import ButtonForm from './buttons/ButtonForm';
 import NotData from './NotData';
 import LendingsItem from "./LendingsItem";
+import Error from './Error';
 
 class LendingsDesactive extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            listDesactive: [],
-            done: false
+            res: false,
+            loading: true,
+            data: [],
+            error: false
         }
         this.submitListDesactive = this.submitListDesactive.bind(this);
     }
@@ -29,30 +32,52 @@ class LendingsDesactive extends React.Component {
         formData.append('year', data[0]);
         formData.append('month', data[1]);
 
-        const fetchPromise = fetch("https://financemeapi.com/api/lending/desactives", {
+        fetch("https://financemeapi.com/api/lending/desactives", {
             method: 'POST',
             'headers': {
                 'Authorization': 'Bearer ' + key,
             },
             body: formData
-        });
+        }, [])
+            .then(response => response.json())
+            .then(res => {
+                if(res['data'] !== null){
+                    this.context.updateListDesactive(res['msg']);
+                } else {
+                    this.context.updateListDesactive([]);
+                }
 
-        fetchPromise.then(response => {
-            return response.json();
-        }).then(res => {
-            this.context.updateListDesactive(res['msg']);
-            this.setState({
-                listDesactive: res['msg'],
-                done: res['res']
+                this.setState({
+                    res: res['res'],
+                    loading: res['res'] ? false : true,
+                    error: res['res'] ? false : true,
+                    data: res['msg'],
+                })
             })
-        })
     }
 
 
     render() {
-        const { done } = this.state;
+        const { res, loading, error, data } = this.state;
         const { submitListDesactive } = this;
         const { listDesactive } = this.context;
+
+
+        if (error) {
+            return (
+                <div>
+                    <div className='overflow-y-auto h-full text-xs p-1' >
+
+                        <form onSubmit={submitListDesactive} className='flex p-3 justify-center items-center rounded-lg bg-white shadow-md shadow-slate-500/20 text-sm' id='formTemplate'>
+                            <input type="month" name="month-year" id="month-year" min="2022-01" className='mr-3' />
+                            <ButtonForm name="Go" />
+                        </form>
+
+                        <Error />
+                    </div>
+                </div>
+            );
+        }
 
         return (
             <div>
@@ -63,13 +88,13 @@ class LendingsDesactive extends React.Component {
                         <ButtonForm name="Go" />
                     </form>
 
-                    {!done ? <NotData /> : (listDesactive.length < 1 ?
+                    { listDesactive.length < 1 ?
                         <NotData />
                         :
                         listDesactive.map(item => (
-                            <LendingsItem  item={item} key={item.id}/>
+                            <LendingsItem item={item} key={item.id} />
                         ))
-                        )
+                    
                     }
                 </div>
             </div>
